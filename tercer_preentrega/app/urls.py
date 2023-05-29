6fd67da758500adfea521f34fff58b3cae038e92
Ignacio Orlanno <ignacioorlanno@gmail.com>
2023-05-28 23:46:09 -0300

    estilos bootstrap, forms

--- tercer_preentrega/app/urls.py
+++ tercer_preentrega/app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from app import views
+from . import views
 
 urlpatterns = [
-    
+    path('', views.inicio, name="Inicio"),
+    path('obras', views.obras, name="Obras"),
+    path('arquitectos', views.obras, name="Arquitectos"),
+    path('barrios', views.obras, name="Barrios"),
+    path('buscar/', views.buscar)
 ]
--- tercer_preentrega/app/views.py
+++ tercer_preentrega/app/views.py
@@ -1,1 +1,26 @@
+from django.shortcuts import render, HttpResponse
+from django.http import HttpResponse
 from app.models import Barrio,Arquitecto,Obra
+
+def inicio(request):
+    return render(request, "app/inicio.html")
+
+def obras(request):
+    return render(request, "app/obras.html")
+
+def arquitectos(request):
+    return render(request, "app/arquitectos.html")
+
+def barrios(request):
+    return render(request, "app/barrios.html")
+
+def buscar(request):
+    if request.GET["barrio"]:
+        barrio = request.GET["barrio"]
+        obras = Obra.objects.filter(barrio__icontains=barrio)
+
+        return render(request, 'app/inicio.html', {"obras":obras, "barrio":barrio})
+    else:
+        response = "No ingresaste barrio"
+
+    return HttpResponse(response)
